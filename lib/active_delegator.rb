efdbe5c76272393f0b3b20fd4b197b9483be23e2
Cameron Martin <cameronmartin123@gmail.com>
2014-01-25 22:46:25 +0000

    Changed ActiveDelegator to ActiveDelegator::Base

--- lib/active_delegator.rb
+++ lib/active_delegator.rb
@@ -2,54 +2,55 @@ require 'active_delegator/version'
 
 require 'active_record'
 
-class ActiveDelegator < ActiveRecord::Base
+module ActiveDelegator
+  class Base < ActiveRecord::Base
 
-  self.abstract_class=true
+    self.abstract_class=true
 
 
-  class << self
+    class << self
 
-    attr_accessor :model_class
+      attr_accessor :model_class
 
-    def maps_to(klass)
-      self.model_class = klass
+      def maps_to(klass)
+        self.model_class = klass
+      end
+
+      def attributes
+        @attributes ||= []
+      end
+
+
+      def attribute(attr)
+        attributes.push(attr)
+      end
+
     end
 
-    def attributes
-      @attributes ||= []
+    attr_accessor :model
+
+    def write_attribute(attr, value)
+      model_instance.public_send("#{attr}=", value)
+      super
     end
 
-
-    def attribute(attr)
-      attributes.push(attr)
+    def model_instance
+      @model ||= self.class.model_class.allocate
     end
 
+    ## Copies attributes from store to model
+    #def set_attributes
+    #  self.class.attributes.each do |attr|
+    #    model_instance.public_send("#{attr}=", read_attribute(attr))
+    #  end
+    #end
+    #
+    ## Copies attributes from model to store
+    #def get_attributes
+    #  self.class.attributes.each do |attr|
+    #    write_attribute(attr, model_instance.public_send(attr))
+    #  end
+    #end
+
   end
-
-  attr_accessor :model
-
-  def write_attribute(attr, value)
-    model_instance.public_send("#{attr}=", value)
-    super
-  end
-
-  def model_instance
-    @model ||= self.class.model_class.allocate
-  end
-
-  ## Copies attributes from store to model
-  #def set_attributes
-  #  self.class.attributes.each do |attr|
-  #    model_instance.public_send("#{attr}=", read_attribute(attr))
-  #  end
-  #end
-  #
-  ## Copies attributes from model to store
-  #def get_attributes
-  #  self.class.attributes.each do |attr|
-  #    write_attribute(attr, model_instance.public_send(attr))
-  #  end
-  #end
-
-
 end
--- lib/active_delegator/version.rb
+++ lib/active_delegator/version.rb
@@ -1,3 +1,3 @@
-class ActiveDelegator
+module ActiveDelegator
   VERSION = '0.0.1'
 end
