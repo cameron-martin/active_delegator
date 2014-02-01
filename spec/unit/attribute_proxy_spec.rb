30d5b6bd318fb34047c7b4b95c6a1919150fab01
Cameron Martin <cameronmartin123@gmail.com>
2014-02-01 13:00:14 +0000

    Added attribute proxy spec

---
+++ spec/unit/attribute_proxy_spec.rb
@@ -0,0 +1,55 @@
+require 'rspec'
+
+require 'active_delegator/attribute_proxy'
+
+describe ActiveDelegator::AttributeProxy do
+
+  let(:model) { double('model') }
+  let(:attribute_proxy) { ActiveDelegator::AttributeProxy.new(model, { :attr1 => :attr2 }) }
+
+  describe '#[]' do
+    it 'delegates mapped attribute getters to model' do
+      model.should_receive(:attr2)
+      attribute_proxy[:attr1]
+    end
+
+    it 'persists internal attributes' do
+      attribute_proxy[:other] = :value
+      expect(attribute_proxy[:other]).to eq(:value)
+    end
+  end
+
+  describe '#[]=' do
+    it 'delegates mapped attribute getters to model' do
+      model.should_receive(:attr2=).with(:value)
+      attribute_proxy[:attr1] = :value
+    end
+
+    it 'handles unmapped attributes internally' do
+      attribute_proxy[:other] = :value
+    end
+  end
+
+  describe '#keys' do
+    it 'returns mapped keys' do
+      expect(attribute_proxy.keys).to eq([:attr1])
+    end
+    it 'returns internal keys as well' do
+      attribute_proxy[:other] = :value
+      expect(attribute_proxy.keys).to eq([:attr1, :other])
+    end
+  end
+
+  describe '#has_key?' do
+    it 'has mapped key' do
+      expect(attribute_proxy.has_key?(:attr1)).to be_true
+    end
+    it 'does not have other key' do
+      expect(attribute_proxy.has_key?(:other)).to be_false
+    end
+    it 'has unmapped key' do
+      attribute_proxy[:other] = :value
+      expect(attribute_proxy.has_key?(:other)).to be_true
+    end
+  end
+end
