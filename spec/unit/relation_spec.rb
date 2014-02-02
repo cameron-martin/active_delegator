70558339320b9e374cabc19e8f909dd6149dde31
Cameron Martin <cameronmartin123@gmail.com>
2014-02-02 08:37:35 +0000

    Added more specs

--- spec/unit/relation_spec.rb
+++ spec/unit/relation_spec.rb
@@ -21,5 +21,51 @@ module ActiveDelegator
         expect(relation.where).to be_a(Relation)
       end
     end
+
+    context 'enumerable' do
+      let(:mapper_object) { double('mapper_object') }
+      let(:store) { double('store') }
+
+      describe '#to_a' do
+
+        it 'delegates to the relation' do
+          ar_relation.stub(:to_a => [])
+          ar_relation.should_receive(:to_a)
+          relation.to_a
+        end
+
+        it 'caches the result' do
+          ar_relation.stub(:to_a => [])
+          ar_relation.should_receive(:to_a).exactly(1).times
+          relation.to_a
+          relation.to_a
+        end
+
+        it 'returns a different object each time' do
+          ar_relation.stub(:to_a => [])
+          expect(relation.to_a).to_not equal(relation.to_a)
+        end
+
+        it 'wraps the stores in mappers' do
+          ar_relation.stub(:to_a => [store] * 2)
+          mapper_class.stub(:new => mapper_object)
+
+          mapper_class.should_receive(:new).exactly(2).times.with(store)
+
+          expect(relation.to_a).to eq([mapper_object] * 2)
+        end
+      end
+
+      describe '#each' do
+        it 'yields mapper objects' do
+          ar_relation.stub(:to_a => [store] * 2)
+          mapper_class.stub(:new => mapper_object)
+
+          mapper_class.should_receive(:new).exactly(2).times.with(store)
+
+          expect { |b| relation.each(&b) }.to yield_successive_args(*([mapper_object] * 2))
+        end
+      end
+    end
   end
 end
